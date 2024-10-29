740730d2256e80ae911ffcefb0674325f59fe769
Mohammed Farshad <imohammedfarshad@gmail.com>
2024-10-29 10:04:45 +0530

    Update Password generator project

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -1,14 +1,38 @@
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 function App() {
   const [length, setLength] = useState(8);
   const [isNumberAllowed, setIsNumberAllowed] = useState(false);
   const [isCharacterAllowed, setIsCharacterAllowed] = useState(false);
+  const [copied, setCopied] = useState(false);
+  const [password, setPassword] = useState("");
 
-  const handleLength = (e) => {
-    const value = e.target.value;
-    setLength(value);
-  };
+  const passwordRef = useRef(null);
+
+  const copyPassword = useCallback(() => {
+    navigator.clipboard.writeText(password).then(() => {
+      setCopied(true);
+      passwordRef.current?.select();
+      passwordRef.current?.setSelectionRange(0, 24);
+      setTimeout(() => setCopied(false), 1000);
+    });
+  }, [password]);
+
+  const passwordGenerator = useCallback(() => {
+    let str = "ABCDFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    let pass = "";
+    if (isNumberAllowed) str += "123456789";
+    if (isCharacterAllowed) str += "!@#$%^&*(){}[]";
+    for (let i = 1; i <= length; i++) {
+      const char = Math.floor(Math.random() * str.length);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+  }, [isNumberAllowed, isCharacterAllowed, length, setPassword]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, isNumberAllowed, isCharacterAllowed, passwordGenerator]);
 
   return (
     <>
@@ -21,20 +45,29 @@ function App() {
             <input
               type="text"
               className="rounded-s-lg h-10 w-[300px] outline-none focus:outline-none p-3"
-              placeholder="Generated Password"
+              placeholder="Generated password"
+              value={password}
+              readOnly
+              ref={passwordRef}
             />
-            <button className="px-4 bg-blue-600 hover:bg-blue-800 rounded-e-lg text-lg text-white font-medium">
-              Copy
+            <button
+              className="px-4 bg-blue-600 hover:bg-blue-800 rounded-e-lg text-lg text-white font-medium"
+              onClick={copyPassword}
+            >
+              {copied ? "Copied" : "Copy"}
             </button>
           </div>
 
           <div className="flex space-x-3 mb-3">
             <input
               type="range"
-              min="8"
-              max="16"
+              min="1"
+              max="24"
               defaultValue={length}
-              onChange={handleLength}
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+              className="cursor-pointer"
             />
             <label
               htmlFor="range "
@@ -44,14 +77,24 @@ function App() {
             </label>
           </div>
           <div className="flex space-x-3">
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              onChange={() => {
+                setIsCharacterAllowed((prev) => !prev);
+              }}
+            />
             <label
               htmlFor="characters"
               className="text-lg text-white font-medium"
             >
               Characters
             </label>
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              onChange={() => {
+                setIsNumberAllowed((prev) => !prev);
+              }}
+            />
             <label htmlFor="numbers" className="text-lg text-white font-medium">
               Numbers
             </label>
